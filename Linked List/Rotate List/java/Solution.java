973ca0ce8d7294a9811def5c18b4f3ac04ef3e89
Sivarooban_k <jksivarooban@gmail.com>
2026-06-26 00:28:00 +0530

    feat: Add Rotate List

--- Linked List/Rotate List/java/Solution.java
+++ Linked List/Rotate List/java/Solution.java
@@ -10,6 +10,7 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null) return head;
         int length =0;
         ListNode temp = head;
         while(temp.next != null){
